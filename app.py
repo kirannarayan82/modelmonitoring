6522e03a7a3a21dbfb09a2479ff080a5059ed3c9
ksathyanarayana <genuser0482@outlook.com>
2024-10-29 17:54:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-import nannyml as nml
+import nannyML as nml
 
 # Simulate some data for demonstration purposes
 def generate_data(n_samples):
